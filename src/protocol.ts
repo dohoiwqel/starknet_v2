d670f992da1ee27749f11aa6d738b4075b2db6d3
badmen87 <dohoiwqel@gmail.com>
2023-09-24 17:40:05 +0300

    fix

--- src/protocol.ts
+++ src/protocol.ts
@@ -20,12 +20,17 @@ export class Protocol {
             const provider = new SequencerProvider({ baseUrl: constants.BaseUrl.SN_MAIN })
             const response = await provider.waitForTransaction(tx, {retryInterval: 1000, successStates: [TransactionStatus.ACCEPTED_ON_L2]})
             
-            if(response.status === 'ACCEPTED_ON_L2') return response;
-            if(response.status === 'REJECTED') throw `Не удалось послать транзакцию ${response.transaction_hash}`;
-
-            return await this.waitForTransaction(tx)
-
+            if(response.status === 'ACCEPTED_ON_L2') {
+                return response
+            
+            } else if(response.status === 'REJECTED') {
+                throw `Не удалось послать транзакцию ${response.transaction_hash}`;
+            }
+             else {
+                return await this.waitForTransaction(tx)
+            }
         } catch(e: any) {
+            console.log('DEV: ошибка с ожиданием транзакции')
             throw (e.response || e.error || e)
         }
     }
@@ -37,7 +42,7 @@ export class Protocol {
             return await this.waitForTransaction(receipt.transaction_hash)
 
         } catch(e: any) {
-            if(e.message && e.message.includes('Invalid transaction nonce')) {
+            if(e.message && e.message.includes('nonce')) {
                 console.log('DEV: Ошибка с nonce')
                 return await this.sendTransaction(contract, functionName, callData)
             }
